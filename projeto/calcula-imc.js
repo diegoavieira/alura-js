33916686981fef3078b860020ae006496b08eb30
Diego Vieira <diegoavieira@gmail.com>
2016-11-22 00:45:37 -0200

    Finish

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -1,15 +1,32 @@
+function calculaTodosImcs() {
+	
+	var trsPacientes = document.getElementsByClassName("paciente");
 
-var trsPacientes = document.getElementsByClassName("paciente");
+	percorreArray(trsPacientes, function (pacienteTr) {
 
-percorreArray(trsPacientes, function (pacienteTr) {
+		var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+		var pacienteAtual = montaPaciente(pacienteTr);
 
-	var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
-	var pacienteAtual = montaPaciente(pacienteTr);
+		var imc = pacienteAtual.pegaImc();
+		tdImc.textContent = imc;
 
-	var imc = pacienteAtual.pegaImc();
-	tdImc.textContent = imc;
+		console.log(imc);
 
-	console.log(imc);
+	});		
+}
 
-});	
+var botao = document.getElementById("calcula-imcs");
+
+// botao.onclick = calculaTodosImcs;
+// botao.onclick = function() {
+// 	console.log("Calculando Imcs");
+// }
+
+botao.addEventListener("click", calculaTodosImcs);
+botao.addEventListener("click", function() {
+	console.log("Calculando Imcs");
+});
+
+
+
 	
